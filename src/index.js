a532878e59919b45205377e01b7633fbaa08ef32
lionel lints <lionel.lints@gmail.com>
2017-04-16 09:08:07 -0700

    add BrowserRouter as top route 🍍

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
 
 import App from './components/App';
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root'),
 );
