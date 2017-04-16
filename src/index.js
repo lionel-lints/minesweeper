3fa7821a34f347a738a02080538a2ba58a30fcdb
lionel lints <lionel.lints@gmail.com>
2017-04-16 09:37:33 -0700

    add basic auth routing to Base.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/index.css';
 
-import App from './components/App';
+import Base from './components/Base';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Base />
   </BrowserRouter>,
   document.getElementById('root'),
 );
